6f1fb55a16526b84c3f2ba49d0223f0b035c4ead
Foomoo <Jonmiami4@gmail.com>
2022-01-08 23:08:40 +0330

    Added files and changes required for deployment to heroku

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -119,7 +119,3 @@ STATIC_URL = "/static/"
 django_heroku.settings(locals())
 
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_databa se_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
